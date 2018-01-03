4a7e3e6da16045a36b9f72da7f0b22831d1820fa
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-03 18:29:59 +0900

    update ABC080

--- code/ProCon/ABC/ABC080/ABC080/ca.cpp
+++ code/ProCon/ABC/ABC080/ABC080/ca.cpp
@@ -1,50 +1,50 @@
-#include<math.h>
-#include<limits.h>
-#include<iostream>
-#include<string>
-#include<vector>
-#include<stdio.h>
-#include<sstream>
-#include<list>
-#include<queue>
-#include<algorithm>
-#include<functional>
-#include<map>
-#include<set>
-#include<utility>
-#include<initializer_list>
-#include<tuple>
-#include<regex>
-using namespace std;
-#define ull unsigned long long
-#define ll long long
-#define rep(i, n) for(int i=0;i<(int)(n);i++)
-#define all(t) t.begin(), t.end()
-
-
-int main() {
-	int n;
-	cin >> n;
-	vector<vector<int>>f(n, vector<int>(10));
-	vector<vector<int>>p(n, vector<int>(11));
-	rep(i, n)rep(j, 10)cin>>f[i][j];
-	rep(i, n)rep(j, 11)cin >> p[i][j];
-	int res = -(1 << 30);
-	for (int b = 1; b < (1 << 10); b++) {
-		int cc = 0;
-		rep(i, n) {
-			int c = 0;
-			rep(j, 10) {
-				if ((b >> j & 1) && f[i][j])c++;
-			}
-			cc += p[i][c];
-		}
-		if (res < cc)res = cc;
-	}
-	cout << res << endl;
-
-	return 0;
-}
-
-
-
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<stdio.h>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, n) for(int i=0;i<(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//
+//
+//int main() {
+//	int n;
+//	cin >> n;
+//	vector<vector<int>>f(n, vector<int>(10));
+//	vector<vector<int>>p(n, vector<int>(11));
+//	rep(i, n)rep(j, 10)cin>>f[i][j];
+//	rep(i, n)rep(j, 11)cin >> p[i][j];
+//	int res = -(1 << 30);
+//	for (int b = 1; b < (1 << 10); b++) {
+//		int cc = 0;
+//		rep(i, n) {
+//			int c = 0;
+//			rep(j, 10) {
+//				if ((b >> j & 1) && f[i][j])c++;
+//			}
+//			cc += p[i][c];
+//		}
+//		if (res < cc)res = cc;
+//	}
+//	cout << res << endl;
+//
+//	return 0;
+//}
+//
+//
+//
--- code/ProCon/ABC/ABC080/ABC080/d.cpp
+++ code/ProCon/ABC/ABC080/ABC080/d.cpp
@@ -1,34 +1,52 @@
-//#include<math.h>
-//#include<limits.h>
-//#include<iostream>
-//#include<string>
-//#include<vector>
-//#include<stdio.h>
-//#include<sstream>
-//#include<list>
-//#include<queue>
-//#include<algorithm>
-//#include<functional>
-//#include<map>
-//#include<set>
-//#include<utility>
-//#include<initializer_list>
-//#include<tuple>
-//#include<regex>
-//using namespace std;
-//#define ull unsigned long long
-//#define ll long long
-//#define rep(i, n) for(int i=0;i<(int)(n);i++)
-//#define all(t) t.begin(), t.end()
-//
-//
-//int main() {
-//	int n, a, b;
-//	cin >> n >> a >> b;
-//	cout << min(a*n, b) << endl;
-//
-//	return 0;
-//}
-//
-//
-//
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+
+
+int main() {
+	int n, C;
+	cin >> n >> C;
+	vector<int>s(n), t(n), c(n);
+	rep(i, n)cin >> s[i] >> t[i] >> c[i];
+	int lim = 2 * 1e5 + 2;
+	vector<int>sum(lim, 0);
+	for (int i = 1; i <= C; i++) {
+		vector<int> d(lim, 0);
+		rep(j, n) {
+			if (c[j] == i) {
+				d[2 * s[j] - 1]++;
+				d[2 * t[j]]--;
+			}
+		}
+		vector<int> cumulativeSum(d);
+		for (int j = 1; j < lim; j++) {
+			cumulativeSum[j] += cumulativeSum[j - 1];
+		}
+		rep(j, lim)if (cumulativeSum[j] > 0) sum[j]++;
+	}
+	int mx = *max_element(all(sum));
+	std::cout << mx << endl;
+	return 0;
+}
+
+
+
