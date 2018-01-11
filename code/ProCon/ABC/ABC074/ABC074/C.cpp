80e38d0b9e626e682b4464c071abdb9a0fd4a4b2
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-11 21:21:03 +0900

    re-solve ABC074C

--- code/ProCon/ABC/ABC074/ABC074/C.cpp
+++ code/ProCon/ABC/ABC074/ABC074/C.cpp
@@ -7,55 +7,47 @@
 #include<sstream>
 #include<list>
 #include<queue>
-#include <algorithm>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
 #include<initializer_list>
+#include<tuple>
+#include<regex>
 using namespace std;
-#define ul unsigned long long
+#define ull unsigned long long
 #define ll long long
-#define rup(i, a, b) for(int (i) = (a); (i) < (b); (i)++)
-#define rdn(i, a, b) for(int (i) = (a); (i) > (b); (i)--)
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
 
-double noudo(int a, int b) {
-	return 100 * b / (a + b);
-}
 
 int main() {
+	// ABC074C AC
 	int a, b, c, d, e, f;
 	cin >> a >> b >> c >> d >> e >> f;
-	list<int> x(0);
-	list<int> y(0);
-	for (int i = 0; i <= f; i++) {
-		for (int j = 0; j <= f; j++) {
-			int tmp = 100 * a*i + 100 * b*j;
-			if (0 < tmp &&tmp <= f) {
-				x.push_back(tmp);
-			}
-		}
-	}
-	rup(i, 0, f + 1) {
-		rup(j, 0, f + 1) {
-			int tmp = c*i + d*j;
-			if (tmp <= f) {
-				y.push_back(tmp);
-			}
-		}
-	}
-	int ansWa = 0;
-	int ansSu = 0;
-	double density = 0;
-	for (auto wa : x) {
-		for (auto su : y) {
-			if (wa + su <= f && (double)su / wa <= (double)e / 100) {
-				double tdens = 100 * su / (wa + su);
-				if (tdens > density) {
-					density = tdens;
-					ansWa = wa;
-					ansSu = su;
+	double ma = 0;
+	int answ, anss;
+	for (int i = 0; 100 * a*i <= f; i++) {
+		for (int j = 0; 100 * b*j <= f; j++) {
+			for (int k = 0; c*k <= f; k++) {
+				for (int l = 0; d*l <= f; l++) {
+					int w = 100 * a*i + 100 * b*j;
+					int s = c*k + d*l;
+					if (w + s <= f && 100 * s <= w*e) {
+						double can = (double)100 * s / (w + s);
+						if (ma <= can) {
+							answ = w + s;
+							anss = s;
+							ma = can;
+						}
+
+					}
 				}
 			}
 		}
 	}
-	cout << ansWa << " " << ansSu;
+	cout << answ<<" "<<anss << endl;
 	return 0;
 }
 
