4b69fa78c21e6df9f9f62fa5bf893d3d4a9c9de2
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-17 22:13:09 +0900

    ABC070b

---
+++ code/ProCon/ABC/ABC070/b.cpp
@@ -0,0 +1,38 @@
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+
+int main() {
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  int n =101;
+  int sum =0;
+  rep(i,n){
+    if(a<=i && i<b && c<=i && i<d) sum++;
+  }
+	cout<<sum;
+	return 0;
+}
+
+
+
---
+++ code/ProCon/ABC/ABC070/b2.cpp
@@ -0,0 +1,36 @@
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+
+int main() {
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  int l = max(a,c);
+  int r = min(b,d);
+  cout<<max(r-l, 0);
+
+	return 0;
+}
+
+
+
