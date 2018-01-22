b38ef539743f0aa98dfc1591ea72c81dd6912080
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-22 22:27:13 +0900

    ABC069bc

---
+++ code/ProCon/ABC/ABC069/ABC069/c.cpp
@@ -0,0 +1,44 @@
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int>a(n);
+	rep(i, n)cin >> a[i];
+	vector<int> b(4, 0);
+	rep(i, n) {
+		b[a[i] % 4]++;
+	}
+	if (b[2] != 1) b[2] = 0;
+	if (b[0] >= b[1] + b[2] + b[3] - 1) cout << "Yes";
+	else cout << "No";
+
+
+
+	return 0;
+}
+
+
+
