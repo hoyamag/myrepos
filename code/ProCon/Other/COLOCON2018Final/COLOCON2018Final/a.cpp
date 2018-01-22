e234e6b6585106085863358fe3a3ea3f19184e70
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-22 15:47:48 +0900

    result of COLOCON2018Final

---
+++ code/ProCon/Other/COLOCON2018Final/COLOCON2018Final/a.cpp
@@ -0,0 +1,32 @@
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+
+int main() {
+
+
+	return 0;
+}
+
+
+
