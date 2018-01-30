6d8729d65ba62ab4db70d3295bb0542e5fe8d256
hoyamag <2036oshmkufafg36@gmail.com>
2018-01-30 22:31:07 +0900

    ABC058b

---
+++ code/ProCon/ABC/ABC058/b.cpp
@@ -0,0 +1,59 @@
+
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, n) for(int i=0;i<(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+
+int main() {
+	string o, e;
+	cin >> o >> e;
+	rep(i, o.length() + e.length()) {
+		if ((i+1) % 2 == 0)cout << e[i/2];
+		else cout << o[i/2];
+	}
+	return 0;
+}
+
+
