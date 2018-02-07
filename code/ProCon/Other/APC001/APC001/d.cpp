6f0a0289e28206ad70878ee2eca09a69b4c2d257
hoyamag <2036oshmkufafg36@gmail.com>
2018-02-07 22:16:06 +0900

    APC001d

--- code/ProCon/Other/APC001/APC001/d.cpp
+++ code/ProCon/Other/APC001/APC001/d.cpp
@@ -31,29 +31,14 @@ void dfs(int &n, vector<list<int>>&g, vector<ll>&a, vector<bool>&chd, int &treen
 
 	}
 }
-void combine(list < pair<ll, int>>&left, list<pair<ll, int>>&right) {
-	auto r = right.begin();
-
-	for (auto l = left.begin(); l != left.end(); l++) {
-		ll le = l->first;
-		ll re = r->first;
-		while (le > re) {
-			left.insert(l, { r->first, r->second });
-			++r;
-		}
-		if (r == right.end())break;
-	}
-	while (r != right.end()) {
-		left.push_back({ r->first, r->second });
-		++r;
-	}
-	right.push_front({ -1,-1 });
-
-}
 int main() {
 	ll lim = 1e9 + 1;
 	int n, m;
 	cin >> n >> m;
+	if (m == n - 1) {
+		cout << 0;
+		return 0;
+	}
 	vector<ll>a(n);
 	rep(i, n)cin >> a[i];
 	vector<list<int>>g(n);
@@ -76,52 +61,29 @@ int main() {
 		}
 	}
 	ll ans = 0;
-	while (treenum > 1) {
-		ll mi = LLONG_MAX;
-		int left = -1;
-		int lefttree;
-		rep(i, treenum) {
-			auto p = t[i].front();
-			if (p.second != -1) {
-				if (p.first < mi) {
-					mi = p.first;
-					left = i;
-				}
-			}
-		}
-		if (left == -1) {
-			cout << "Impossible";
-			return 0;
-		}
-
-		ll summin = LLONG_MAX;
-		int right = -1;
-		rep(i, treenum) {
-			if (i != left) {
-				pair<ll, int> p = t[i].front();
-				if (p.second != -1) {
-					auto p2 = (++t[i].begin())->first;
-					ll c = p.first + p2;
-					if (c < summin) {
-						summin = c;
-						right = i;
-
-					}
-				}
-			}
-		}
-		if (right == -1) {
-			cout << "Impossible";
-			return 0;
-		}
-		ans += t[left].begin()->first + t[right].begin()->first;
-		t[left].erase(t[left].begin());
-		t[right].erase(t[right].begin());
-		combine(t[left], t[right]);
-		treenum--;
+	int usedNum = 0;
+	rep(i, treenum) {
+		ans += t[i].front().first;
+		t[i].pop_front();
+		usedNum++;
 	}
+	priority_queue<ll, vector<ll>, greater<ll> >costbox;
+	rep(i, treenum) {
+		auto it = t[i].begin();
+		while (it != t[i].end()) {
+			costbox.push(it->first);
+			it++;
+		}
+	}
+	while (usedNum < 2 * (n - m - 1) && !costbox.empty()) {
+		auto combiningNode = costbox.top();
+		costbox.pop();
+		ans += combiningNode;
+		usedNum++;
 
-	cout << ans;
+	}
+	if (usedNum != 2 * (n - m - 1) )cout << "Impossible";
+	else cout << ans;
 	return 0;
 }
 
