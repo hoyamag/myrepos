10dc8bb1af93768a2a435b83a09b20d483efb644
hoyamag <2036oshmkufafg36@gmail.com>
2018-03-18 22:24:06 +0900

    ABC090

---
+++ code/ProCon/ABC/ABC090/ABC090/b.cpp
@@ -0,0 +1,54 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<stdio.h>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, n) for(int i=0;i<(int)(n);i++)
+//#define rep1n(i, n) for(int i=1;i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//bool is(int n) {
+//	string s = to_string(n);
+//	int k = s.length();
+//	rep(i, s.length() / 2) {
+//		if (s[i] != s[k - i - 1]) return 0;
+//	}
+//	return 1;
+//}
+//
+//int main() {
+//	int a, b;
+//	cin >> a >> b;
+//	int d = b - a;
+//	int cnt = 0;
+//	for (int i = a; i <= b; i++) {
+//		if (is(i))cnt++;
+//		
+//	}
+//	cout << cnt;
+//
+//
+//	return 0;
+//}
+//
+//
+//
---
+++ code/ProCon/ABC/ABC090/ABC090/c.cpp
@@ -0,0 +1,48 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<stdio.h>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, n) for(int i=0;i<(int)(n);i++)
+//#define rep1n(i, n) for(int i=1;i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//int main() {
+//	ll n, m;
+//	cin >> n >> m;
+//	ll ans;
+//	if (n > m)swap(n, m);
+//	if (n == 1) {
+//		if (m == 1) ans = 1;
+//		else ans = max((ll)0, m - 2);
+//	}
+//	else {
+//		if (n - 2 > 0 && m - 2 > 0) ans = max(0LL, (n - 2)*(m - 2));
+//		else ans = 0;
+//	}
+//	cout << ans << endl;
+//
+//	return 0;
+//}
+//
+//
+//
