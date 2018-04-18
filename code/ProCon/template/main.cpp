299cdcceecb3631ba7120aba46a7104b44e1e6c8
hoyamag <2036oshmkufafg36@gmail.com>
2018-04-18 22:08:34 +0900

    update template/main.cpp

--- code/ProCon/template/main.cpp
+++ code/ProCon/template/main.cpp
@@ -4,7 +4,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<stdio.h>
+#include<cstdio>
 #include<sstream>
 #include<list>
 #include<queue>
@@ -19,15 +19,15 @@
 using namespace std;
 #define ull unsigned long long
 #define ll long long
-#define rep(i, n) for(int i=0;i<(int)(n);i++)
-#define rep1n(i, n) for(int i=1;i<=(int)(n);i++)
+#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
 #define all(t) t.begin(), t.end()
 #define rall(t) t.rbegin(), t.rend()
 #define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
 #define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
 #define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+#define DBG(str) cerr<<(str)<<endl;
 int main() {
-	
 	
 	return 0;
 }
