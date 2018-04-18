de86bf4b291a13e872548762f11dbcdf01804480
hoyamag <2036oshmkufafg36@gmail.com>
2018-04-18 22:11:27 +0900

    ARC092

---
+++ code/ProCon/ARC/ARC092/ARC092/b.cpp
@@ -0,0 +1,57 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<stdio.h>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, n) for(int i=0;i<(int)(n);i++)
+//#define rep1n(i, n) for(int i=1;i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//int main() {
+//	int n;
+//	cin >> n;
+//	map<string, int>s;
+//	rep(i, n) {
+//		string a;
+//		cin >> a;
+//		s[a]++;
+//	}
+//	int m;
+//	cin >> m;
+//	map<string, int>t;
+//	rep(i, m) {
+//		string a;
+//		cin >> a;
+//		t[a]++;
+//	}
+//	int ma = 0;
+//	for (auto v : s) {
+//		ma = max(ma, v.second - t[v.first]);
+//	}
+//	cout << ma;
+//
+//
+//	return 0;
+//}
+//
+//
+//
