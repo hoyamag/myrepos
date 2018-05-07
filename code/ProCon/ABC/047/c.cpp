e99e79e80b4dd4ef81d50f770aeafa6f39d4311c
hoyamag <2036oshmkufafg36@gmail.com>
2018-05-07 21:14:53 +0900

    update ABC047

--- code/ProCon/ABC/047/c.cpp
+++ code/ProCon/ABC/047/c.cpp
@@ -29,25 +29,16 @@ using namespace std;
 #define YES(cond) cout << (cond ? "YES" : "NO") << endl;
 #define DBG(str) cerr << (str) << endl;
 int main() {
-  int w, h, n;
-  cin >> w >> h >> n;
-  vector<int> x(n), y(n), a(n);
-  rep(i, 0, n) cin >> x[i] >> y[i] >> a[i];
-  int a1, a2, a3, a4;
-  a1 = a3 = 0;
-  a2 = w;
-  a4 = h;
-  rep(i, 0, n) {
-    if (a[i] == 1)
-      a1 = max(a1, x[i]);
-    if (a[i] == 2)
-      a2 = min(a2, x[i]);
-    if (a[i] == 3)
-      a3 = max(a3, y[i]);
-    if (a[i] == 4)
-      a4 = min(a4, y[i]);
-  }
-  cout << max(a2 - a1, 0) * max(a4 - a3, 0);
 
+  string s;
+  cin >> s;
+  char b = 'B', w = 'W';
+  int n = s.length();
+  int cnt = 0;
+  rep(i, 1, n) {
+    if (s[i] != s[i - 1])
+      cnt++;
+  }
+  cout << cnt;
   return 0;
 }
--- code/ProCon/ABC/047/d.cpp
+++ code/ProCon/ABC/047/d.cpp
@@ -29,25 +29,20 @@ using namespace std;
 #define YES(cond) cout << (cond ? "YES" : "NO") << endl;
 #define DBG(str) cerr << (str) << endl;
 int main() {
-  int w, h, n;
-  cin >> w >> h >> n;
-  vector<int> x(n), y(n), a(n);
-  rep(i, 0, n) cin >> x[i] >> y[i] >> a[i];
-  int a1, a2, a3, a4;
-  a1 = a3 = 0;
-  a2 = w;
-  a4 = h;
-  rep(i, 0, n) {
-    if (a[i] == 1)
-      a1 = max(a1, x[i]);
-    if (a[i] == 2)
-      a2 = min(a2, x[i]);
-    if (a[i] == 3)
-      a3 = max(a3, y[i]);
-    if (a[i] == 4)
-      a4 = min(a4, y[i]);
+  int n, t;
+  cin >> n >> t;
+  vector<ll> a(n);
+  rep(i, 0, n) cin >> a[i];
+  ll mi = a[0];
+  vector<ll> b(n, 0);
+  rep(i, 1, n) {
+    b[i] = a[i] - mi;
+    mi = min(mi, a[i]);
   }
-  cout << max(a2 - a1, 0) * max(a4 - a3, 0);
+  sort(all(b));
+  ll ma = b[n - 1];
+  int ans = upper_bound(all(b), ma) - lower_bound(all(b), ma);
+  cout << ans;
 
   return 0;
 }
