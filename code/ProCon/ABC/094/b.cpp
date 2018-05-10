364dadaf0bb4b98fb4937b85f64e0dfdd9d64add
hoyamag <2036oshmkufafg36@gmail.com>
2018-05-10 20:06:21 +0900

    ABC094b

---
+++ code/ProCon/ABC/094/b.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <bitset>
+#include <cstdio>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, a, n) for (int i = (a); i < (int)(n); i++)
+#define repc(i, a, n) for (int i = (a); i <= (int)(n); i++)
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define mat(type, row, col, init)                                              \
+  vector<vector<type>>(row, vector<type>(col, init));
+#define Yes(cond) cout << (cond ? "Yes" : "No") << endl;
+#define YES(cond) cout << (cond ? "YES" : "NO") << endl;
+#define DBG(str) cerr << (str) << endl;
+int main() {
+  int n, m, x;
+  cin >> n >> m >> x;
+  vector<int> a(m);
+  rep(i, 0, m) cin >> a[i];
+  sort(all(a));
+  int p = 0;
+  rep(i, 0, m) {
+    if (a[i] < x)
+      p = i + 1;
+  }
+  cout << min(p, m - p);
+
+  return 0;
+}
