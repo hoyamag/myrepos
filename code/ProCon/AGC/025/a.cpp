2a13eec094e96a47dd9a41426a639f35f97005d8
hoyamag <2036oshmkufafg36@gmail.com>
2018-06-05 22:55:48 +0900

    AGC025a

--- code/ProCon/AGC/025/a.cpp
+++ code/ProCon/AGC/025/a.cpp
@@ -1,54 +1,44 @@
-#include<bitset>
-#include<math.h>
-#include<limits.h>
-#include<iostream>
-#include<string>
-#include<vector>
-#include<cstdio>
-#include<sstream>
-#include<list>
-#include<queue>
-#include<algorithm>
-#include<functional>
-#include<map>
-#include<set>
-#include<utility>
-#include<initializer_list>
-#include<tuple>
-#include<regex>
+#include <algorithm>
+#include <bitset>
+#include <cstdio>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <limits.h>
+#include <list>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 using namespace std;
 #define ull unsigned long long
 #define ll long long
-#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
-#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+#define rep(i, a, n) for (int i = (a); i < (int)(n); i++)
+#define repc(i, a, n) for (int i = (a); i <= (int)(n); i++)
 #define all(t) t.begin(), t.end()
 #define rall(t) t.rbegin(), t.rend()
-#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
-#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
-#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
-#define DBG(str) cerr<<(str)<<endl;
+#define mat(type, row, col, init)                                              \
+  vector<vector<type>>(row, vector<type>(col, init));
+#define Yes(cond) cout << (cond ? "Yes" : "No") << endl;
+#define YES(cond) cout << (cond ? "YES" : "NO") << endl;
+#define DBG(str) cerr << (str) << endl;
 int ketawa(int n) {
-	int cnt = 0;
-	while (n > 0) {
-		cnt += n % 10;
-		n /= 10;
-
-	}
-	return cnt;
+  int cnt = 0;
+  while (n > 0) {
+    cnt += n % 10;
+    n /= 10;
+  }
+  return cnt;
 }
 int main() {
-	int n;
-	cin >> n;
-	int mi = INT32_MAX;
-	repc(i, 1, n/2+1) {
-		int a = n - i;
-		int b = i;
-		mi = min(mi, ketawa(a) + ketawa(b));
-
-	}
-	cout << mi;
-	return 0;
+  int n;
+  cin >> n;
+  cout << (n % 10 == 0 ? 10 : ketawa(n));
+  return 0;
 }
-
-
-
