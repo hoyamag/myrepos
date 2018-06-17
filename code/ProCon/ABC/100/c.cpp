24bcb3beaff9e8cd05fab335911998c9b6b91b0d
hoyamag <2036oshmkufafg36@gmail.com>
2018-06-17 23:13:30 +0900

    Add note of ABC100

--- code/ProCon/ABC/100/c.cpp
+++ code/ProCon/ABC/100/c.cpp
@@ -1,49 +1,49 @@
-//#include<bitset>
-//#include<math.h>
-//#include<limits.h>
-//#include<iostream>
-//#include<string>
-//#include<vector>
-//#include<cstdio>
-//#include<sstream>
-//#include<list>
-//#include<queue>
-//#include<algorithm>
-//#include<functional>
-//#include<map>
-//#include<set>
-//#include<utility>
-//#include<initializer_list>
-//#include<tuple>
-//#include<regex>
-//using namespace std;
-//#define ull unsigned long long
-//#define ll long long
-//#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
-//#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
-//#define all(t) t.begin(), t.end()
-//#define rall(t) t.rbegin(), t.rend()
-//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
-//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
-//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
-//#define DBG(str) cerr<<(str)<<endl;
-//int main() {
-//	int n;
-//	cin >> n;
-//	vector<ll> a(n);
-//	rep(i, 0, n)cin >> a[i];
-//	int cnt = 0;
-//	rep(i, 0, n) {
-//		while (a[i] >= 2 &&a[i] % 2 == 0) {
-//			a[i] /= 2;
-//			cnt++;
-//		}
-////		cerr << cnt << endl;
-//
-//	}
-//	cout << cnt << endl;
-//	return 0;
-//}
-//
-//
-//
+#include<bitset>
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cstdio>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+#define DBG(str) cerr<<(str)<<endl;
+int main() {
+	int n;
+	cin >> n;
+	vector<ll> a(n);
+	rep(i, 0, n)cin >> a[i];
+	int cnt = 0;
+	rep(i, 0, n) {
+		while (a[i] >= 2 &&a[i] % 2 == 0) {
+			a[i] /= 2;
+			cnt++;
+		}
+//		cerr << cnt << endl;
+
+	}
+	cout << cnt << endl;
+	return 0;
+}
+
+
+
