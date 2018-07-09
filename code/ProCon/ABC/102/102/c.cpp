5aa0dcc28d6b7823173bd571d9f50f24bc0abcf9
hoyamag <2036oshmkufafg36@gmail.com>
2018-07-09 20:26:20 +0900

    ABC102

---
+++ code/ProCon/ABC/102/102/c.cpp
@@ -0,0 +1,54 @@
+#include<bitset>
+#include<math.h>
+#include<limits.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cstdio>
+#include<sstream>
+#include<list>
+#include<queue>
+#include<algorithm>
+#include<functional>
+#include<map>
+#include<set>
+#include<utility>
+#include<initializer_list>
+#include<tuple>
+#include<regex>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+#define DBG(str) cerr<<(str)<<endl;
+
+
+int main() {
+	int n;
+	cin >> n;
+	vector<ll>a(n);
+	rep(i, 0, n) {
+		cin >> a[i];
+		a[i] -= (i + 1);
+	}
+	sort(all(a));
+	int b = a[n / 2];
+	ll sum = 0;
+	rep(i, 0, n) {
+		sum += abs(a[i] - b);
+	}
+	cout << sum;
+
+
+
+	return 0;
+}
+
+
+
