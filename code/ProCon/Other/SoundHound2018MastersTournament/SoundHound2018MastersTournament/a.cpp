ae9373a24533ac706531b4f625c88749b7ca1f94
hoyamag <2036oshmkufafg36@gmail.com>
2018-07-09 20:27:40 +0900

    SoundHound2018MastersTournament2018

---
+++ code/ProCon/Other/SoundHound2018MastersTournament/SoundHound2018MastersTournament/a.cpp
@@ -0,0 +1,40 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<cstdio>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+//#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//#define DBG(str) cerr<<(str)<<endl;
+//int main() {
+//	int a, b;
+//	cin >> a >> b;
+//	if (a + b == 15)cout << '+';
+//	else if (a * b == 15)cout << '*';
+//	else cout << 'x';
+//	return 0;
+//}
+//
+//
+//
---
+++ code/ProCon/Other/SoundHound2018MastersTournament/SoundHound2018MastersTournament/b.cpp
@@ -0,0 +1,46 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<cstdio>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+//#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//#define DBG(str) cerr<<(str)<<endl;
+//int main() {
+//	string s;
+//	int w;
+//	cin >> s;
+//	cin >> w;
+//	int n = s.length();
+//	int j = 0;
+//	rep(i, 0, (int)ceil((double)n/w)) {
+//		cout << s[j];
+//		j += w;
+//
+//	}
+//	return 0;
+//}
+//
+//
+//
