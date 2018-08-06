a6f81dfddc936d37bc0dbedda3219c80970047a5
hoyamag <2036oshmkufafg36@gmail.com>
2018-08-06 17:41:11 +0900

    ABC104

---
+++ code/ProCon/ABC/104/104/b.cpp
@@ -0,0 +1,58 @@
+//#include<bitset>
+//#include<math.h>
+//#include<limits.h>
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<cstdio>
+//#include<sstream>
+//#include<list>
+//#include<queue>
+//#include<algorithm>
+//#include<functional>
+//#include<map>
+//#include<set>
+//#include<utility>
+//#include<initializer_list>
+//#include<tuple>
+//#include<regex>
+//using namespace std;
+//#define ull unsigned long long
+//#define ll long long
+//#define rep(i, a, n) for(int i=(a);i<(int)(n);i++)
+//#define repc(i, a, n) for(int i=(a);i<=(int)(n);i++)
+//#define all(t) t.begin(), t.end()
+//#define rall(t) t.rbegin(), t.rend()
+//#define mat(type, row, col, init) vector<vector<type>>(row, vector<type>(col, init));
+//#define Yes(cond) cout<<(cond? "Yes":"No")<<endl;
+//#define YES(cond) cout<<(cond? "YES":"NO")<<endl;
+//
+//#define DBG(str) cerr<<(str)<<endl;
+//bool lc(char a) {
+//	return 'a' <= a&&a <= 'z';
+//}
+//int main() {
+//	string s;
+//	cin >> s;
+//	string ss = s.substr(2, s.length() - 3);
+//	if (s[0] == 'A'&&lc(s[1]) && lc(s[s.length() - 1])) {
+//		int ccount = 0;
+//		int capitalCount = 0;
+//		rep(i, 2, s.length()-1) {
+//			if (!lc(s[i])) {
+//				++capitalCount;
+//				if (s[i] == 'C')++ccount;
+//			}
+//		}
+//		if (ccount == 1 && capitalCount == 1)cout << "AC";
+//		else cout << "WA";
+//
+//	}
+//	else {
+//		cout << "WA";
+//	}
+//	return 0;
+//}
+//
+//
+//
